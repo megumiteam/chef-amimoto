d3a2b168e8797fd0df64bf578e3d7e5a81a529b6
wokamoto <wokamoto1973@gmail.com>
2013-10-09 15:34:54 +0900

    modified midlewares.rb

--- attributes/middlewares.rb
+++ attributes/middlewares.rb
@@ -1,15 +1,58 @@
 ## Nginx
 default[:nginx][:config][:user] = 'nginx'
 default[:nginx][:config][:group] = 'nginx'
-default[:nginx][:config][:worker_processes] = "2"
+default[:nginx][:config][:worker_processes] = '2'
 
 ## PHP
+default[:php][:config][:user] = 'nginx'
+default[:php][:config][:group] = 'nginx'
+default[:php][:config][:max_children] = '5'
+default[:php][:config][:start_servers] = '1'
+default[:php][:config][:min_spare_servers] = '1'
+default[:php][:config][:max_spare_servers] = '4'
+default[:php][:config][:max_requests] = '200'
 
 ## MySQL
+default[:mysql][:config][:user] = 'mysql'
+default[:mysql][:config][:innodb_buffer_pool_size] = '64M'
+default[:mysql][:config][:query_cache_size] = '64M'
+default[:mysql][:config][:tmp_table_size] => '64M'
+default[:mysql][:config][:max_connections] = '128'
+default[:mysql][:config][:thread_cache] = '128'
 
 case node[:ec2][:instance_type]
 when "t1.micro"
-	default[:nginx][:config][:worker_processes] = "2"
+  ## Nginx
+  default[:nginx][:config][:worker_processes] = '2'
+
+  ## PHP
+  default[:php][:config][:max_children] = '5'
+  default[:php][:config][:start_servers] = '1'
+  default[:php][:config][:min_spare_servers] = '1'
+  default[:php][:config][:max_spare_servers] = '4'
+  default[:php][:config][:max_requests] = '200'
+
+  ## MySQL
+  default[:mysql][:config][:innodb_buffer_pool_size] = '64M'
+  default[:mysql][:config][:query_cache_size] = '64M'
+  default[:mysql][:config][:tmp_table_size] => '64M'
+  default[:mysql][:config][:max_connections] = '128'
+  default[:mysql][:config][:thread_cache] = '128'
 when "m1.large"
-	default[:nginx][:config][:worker_processes] = "4"
+  ## Nginx
+  default[:nginx][:config][:worker_processes] = '4'
+
+  ## PHP
+  default[:php][:config][:max_children] = '5'
+  default[:php][:config][:start_servers] = '1'
+  default[:php][:config][:min_spare_servers] = '1'
+  default[:php][:config][:max_spare_servers] = '4'
+  default[:php][:config][:max_requests] = '200'
+
+  ## MySQL
+  default[:mysql][:config][:innodb_buffer_pool_size] = '128M'
+  default[:mysql][:config][:query_cache_size] = '128M'
+  default[:mysql][:config][:tmp_table_size] => '128M'
+  default[:mysql][:config][:max_connections] = '128'
+  default[:mysql][:config][:thread_cache] = '128'
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,53 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-instance_type = node[:ec2][:instance_type]
-
 include_recipe 'amimoto::timezone'
 
-case instance_type
-when "t1.micro"
-	php_max_children = "5"
-	php_start_servers = "1"
-	php_min_spare_servers = "1"
-	php_max_spare_servers = "4"
-	php_max_requests = "200"
-	mysql_innodb_buffer_pool_size = "64M"
-	mysql_query_cache_size = "64M"
-	mysql_tmp_table_size = "64M"
-	mysql_tmp_table_size = "64M"
-	mysql_max_connections = "128"
-	mysql_thread_cache = "128"
-when "m1.large"
-	php_max_children = "5"
-	php_start_servers = "1"
-	php_min_spare_servers = "1"
-	php_max_spare_servers = "4"
-	php_max_requests = "200"
-	mysql_innodb_buffer_pool_size = "128M"
-	mysql_query_cache_size = "128M"
-	mysql_tmp_table_size = "128M"
-	mysql_tmp_table_size = "128M"
-	mysql_max_connections = "128"
-	mysql_thread_cache = "128"
-else
-	php_max_children = "5"
-	php_start_servers = "1"
-	php_min_spare_servers = "1"
-	php_max_spare_servers = "4"
-	php_max_requests = "200"
-	mysql_innodb_buffer_pool_size = "64M"
-	mysql_query_cache_size = "64M"
-	mysql_tmp_table_size = "64M"
-	mysql_tmp_table_size = "64M"
-	mysql_max_connections = "128"
-	mysql_thread_cache = "128"
-end
-
 %w{ memcached zip unzip wget iptables git }.each do | pkg |
-	package pkg do
-		action [:install, :upgrade]
-	end
+  package pkg do
+    action [:install, :upgrade]
+  end
 end
 
 # Percona install
@@ -65,162 +24,146 @@ package "percona-release" do
   provider Chef::Provider::Package::Rpm
 end
 %w{ Percona-Server-server-55 Percona-Server-client-55 Percona-Server-shared-compat }.each do |package_name|
-	package package_name do
-		action [:install, :upgrade]
-	end
+  package package_name do
+    action [:install, :upgrade]
+  end
 end
 
 # nginx install
 service "httpd" do
-	action [:stop, :disable]
+  action [:stop, :disable]
 end
 package "nginx" do
-	action [:install, :upgrade]
+  action [:install, :upgrade]
 end
 
 # php54 install
 %w{ php54 php54-cli php54-fpm php54-devel php54-mbstring php54-gd php-pear php54-xml php54-mcrypt php54-mysqlnd php54-pdo php54-pecl-apc php54-pecl-memcache }.each do | pkg |
-	package pkg do
-		action [:install, :upgrade]
-	end
+  package pkg do
+    action [:install, :upgrade]
+  end
 end
 
 # nginx install
 service "httpd" do
-	action [:stop, :disable]
+  action [:stop, :disable]
 end
 
 package "nginx" do
-	action [:install, :upgrade]
+  action [:install, :upgrade]
 end
 
 # configure mysql
 service "mysql" do
-	action :stop
+  action :stop
 end
 
 %w{ ib_logfile0 ib_logfile1 }.each do | file_name |
-	file "/var/lib/mysql/" + file_name do
-		action :delete
-	end
+  file "/var/lib/mysql/" + file_name do
+    action :delete
+  end
 end
 
 template "/etc/my.cnf" do
-	variables(
-		:innodb_buffer_pool_size => mysql_innodb_buffer_pool_size,
-		:query_cache_size => mysql_query_cache_size,
-		:tmp_table_size => mysql_tmp_table_size,
-		:max_connections => mysql_max_connections,
-		:thread_cache => mysql_thread_cache
-	)
-	source "my.cnf.erb"
+  variables node[:mysql][:config]
+  source "my.cnf.erb"
 end
 
 service "mysql" do
-	action [:enable, :start]
+  action [:enable, :start]
 end
 
 # configure nginx
 template "/etc/nginx/nginx.conf" do
-	variables node[:nginx][:config]
-	source "nginx.conf.erb"
+  variables node[:nginx][:config]
+  source "nginx.conf.erb"
 end
 
 %w{ drop expires mobile-detect phpmyadmin wp-multisite-subdir wp-singlesite }.each do | file_name |
-	template "/etc/nginx/" + file_name do
-		source file_name + ".erb"
-	end
+  template "/etc/nginx/" + file_name do
+    source file_name + ".erb"
+  end
 end
 
 %w{ default.conf default.backend.conf }.each do | file_name |
-	template "/etc/nginx/conf.d/" + file_name do
-		variables(
-<<<<<<< HEAD
-			:server_naem => node[:ec2][:instance_id]
-=======
-			:server_name => "default"
->>>>>>> d1cf6492afc4528708371d9c6f0458b9007199ef
-		)
-		source file_name + ".erb"
-	end
+  template "/etc/nginx/conf.d/" + file_name do
+    variables(
+      :server_name => node[:ec2][:instance_id]
+    )
+    source file_name + ".erb"
+  end
 end
 
 %w{ /var/cache/nginx /var/log/nginx /var/www/vhosts }.each do | dir_name |
-	directory dir_name do
-		owner node[:nginx][:user]
-		group node[:nginx][:group]
-		mode 00755
-		recursive true
-		action :create
-	end
+  directory dir_name do
+    owner node[:nginx][:config][:user]
+    group node[:nginx][:config][:group]
+    mode 00755
+    recursive true
+    action :create
+  end
 end
 
 service "nginx" do
-	action [:enable, :restart]
+  action [:enable, :restart]
 end
 
 # configure php
 %w{ apc.ini memcache.ini }.each do | file_name |
-	template "/etc/php.d/" + file_name do
-		source file_name + ".erb"
-	end
+  template "/etc/php.d/" + file_name do
+    source file_name + ".erb"
+  end
 end
 
 template "/etc/php.ini" do
-	source "php.ini.erb"
+  source "php.ini.erb"
 end
 
 template "/etc/php-fpm.conf" do
-	source "php-fpm.conf.erb"
+  source "php-fpm.conf.erb"
 end
 
 template "/etc/php-fpm.d/www.conf" do
-	variables(
-		:max_children => php_max_children,
-		:start_servers => php_start_servers,
-		:min_spare_servers => php_min_spare_servers,
-		:max_spare_servers => php_max_spare_servers,
-		:max_requests => php_max_requests
-	)
-	source "www.conf.erb"
+  variables node[:php][:config]
+  source "www.conf.erb"
 end
 
 %w{ /var/tmp/php/session /var/log/php-fpm }.each do | dir_name |
-	directory dir_name do
-		owner node[:php][:user]
-		group node[:php][:group]
-		mode 00755
-		recursive true
-		action :create
-	end
+  directory dir_name do
+    owner node[:php][:config][:user]
+    group node[:php][:config][:group]
+    mode 00755
+    recursive true
+    action :create
+  end
 end
 
 service "php-fpm" do
-	action [:enable, :restart]
+  action [:enable, :restart]
 end
 
 # install wp-cli
 directory node[:wpcli][:dir] do
-	recursive true
+  recursive true
 end
 
 remote_file "#{node[:wpcli][:dir]}/installer.sh" do
-	source 'http://wp-cli.org/installer.sh'
-	mode 0755
-	action :create_if_missing
+  source 'http://wp-cli.org/installer.sh'
+  mode 0755
+  action :create_if_missing
 end
 
 bin = ::File.join(node[:wpcli][:dir], 'bin', 'wp')
 
 bash 'install wp-cli' do
-	code './installer.sh'
-	cwd node[:wpcli][:dir]
-	environment 'INSTALL_DIR' => node[:wpcli][:dir],
-	            'VERSION' => node[:wpcli][:version]
-	creates bin
+  code './installer.sh'
+  cwd node[:wpcli][:dir]
+  environment 'INSTALL_DIR' => node[:wpcli][:dir],
+              'VERSION' => node[:wpcli][:version]
+  creates bin
 end
 
 link node[:wpcli][:link] do
-	to bin
+  to bin
 end
 
