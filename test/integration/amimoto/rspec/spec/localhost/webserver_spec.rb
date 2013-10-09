08de2558699671eb3c009d24f709bcd052cd1cb8
sawanoboly <sawanoboriyu@higanworks.com>
2013-10-09 21:48:06 +0900

    add test file contain

--- test/integration/amimoto/rspec/spec/localhost/webserver_spec.rb
+++ test/integration/amimoto/rspec/spec/localhost/webserver_spec.rb
@@ -15,3 +15,24 @@ end
 describe command('nginx -t') do
   it { should return_exit_status 0 }
 end
+
+describe file('/etc/nginx/nginx.conf') do
+  it { should be_file }
+  case @@ohaidata[:ec2][:instance_type]
+  when 't1.micro'
+    it { should contain /^\s*worker_processes\s*2/ }
+  when 'm1.large'
+    it { should contain /^\s*worker_processes\s*2/ }
+  end
+end
+
+
+describe file('/etc/php-fpm.d/www.conf') do
+  it { should be_file }
+  case @@ohaidata[:ec2][:instance_type]
+  when 't1.micro'
+    it { should contain /^\s*pm.max_children\s*=\s5/ }
+  when 'm1.large'
+    it { should contain /^\s*pm.max_children\s*=\s20/ }
+  end
+end
