cc819cadd4da51159ca11007f977237d481b3a98
wokamoto <wokamoto1973@gmail.com>
2014-02-20 20:14:34 +0900

    modified nginx.conf template

--- attributes/middlewares.rb
+++ attributes/middlewares.rb
@@ -10,6 +10,8 @@ default[:nginx][:config][:group] = 'nginx'
 default[:nginx][:config][:worker_processes] = '2'
 default[:nginx][:config][:client_max_body_size] = '4M'
 default[:nginx][:config][:proxy_read_timeout] = '90'
+default[:nginx][:config][:worker_rlimit_nofile] = '10240'
+default[:nginx][:config][:worker_connections] = '8192'
 
 ## PHP
 default[:php][:packages] = %w{ php54 php54-cli php54-fpm php54-devel php54-mbstring php54-gd php-pear php54-xml php54-mcrypt php54-mysqlnd php54-pdo php54-pecl-apc php54-pecl-memcache }
