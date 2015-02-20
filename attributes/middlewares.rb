c51c827a039cc9a956cf3ef937305649edc4e06a
wokamoto <wokamoto1973@gmail.com>
2015-02-20 11:27:53 +0900

    fix nginx/mobile-detect

--- attributes/middlewares.rb
+++ attributes/middlewares.rb
@@ -13,6 +13,9 @@ default[:nginx][:config][:proxy_read_timeout] = '90'
 default[:nginx][:config][:worker_rlimit_nofile] = '10240'
 default[:nginx][:config][:worker_connections] = '8192'
 ##default[:nginx][:config][:phpmyadmin_enable] = true
+default[:nginx][:config][:UA_ktai] = '(DoCoMo|J-PHONE|Vodafone|MOT-|UP\.Browser|DDIPOCKET|ASTEL|PDXGW|Palmscape|Xiino|sharp pda browser|Windows CE|L-mode|WILLCOM|SoftBank|Semulator|Vemulator|J-EMULATOR|emobile|mixi-mobile-converter|PSP)'
+default[:nginx][:config][:UA_smartphone] ='(iPhone|iPod|incognito|webmate|Android|dream|CUPCAKE|froyo|BlackBerry|webOS|s8000|bada|IEMobile|Googlebot\-Mobile|AdsBot\-Google)'
+default[:nginx][:config][:UA_smartphone_off] ='wptouch[^\\=]+\\=(normal|desktop)'
 
 ## PHP
 default[:php][:packages] = %w{ php54 php54-cli php54-fpm php54-devel php54-mbstring php54-gd php-pear php54-xml php54-mcrypt php54-mysqlnd php54-pdo php54-pecl-apc php54-pecl-memcache }
